eb49e20038a027a801c2d6c1ab3074f253599624
ELENA_BURGER <elena96b@gmail.com>
2023-02-19 13:15:33 -0500

    Commit up to 4

--- packages/client/src/GameBoard.tsx
+++ packages/client/src/GameBoard.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useComponentValue } from "@latticexyz/react";
 import { useMUD } from "./MUDContext";
 
@@ -12,6 +13,41 @@ export const GameBoard = () => {
   } = useMUD();
 
   const playerPosition = useComponentValue(Position, playerEntity);
+
+  useEffect(() => {
+    const moveTo = async (x: number, y: number) => {
+      const tx = await systems["system.Move"].executeTyped({ x, y });
+      await tx.wait();
+    };
+
+    const moveBy = async (deltaX: number, deltaY: number) => {
+      if (!playerPosition) {
+        console.warn(
+          "cannot moveBy without a player position, not yet spawned?"
+        );
+        return;
+      }
+      await moveTo(playerPosition.x + deltaX, playerPosition.y + deltaY);
+    };
+
+    const listener = (e: KeyboardEvent) => {
+      if (e.key === "ArrowUp") {
+        moveBy(0, -1);
+      }
+      if (e.key === "ArrowDown") {
+        moveBy(0, 1);
+      }
+      if (e.key === "ArrowLeft") {
+        moveBy(-1, 0);
+      }
+      if (e.key === "ArrowRight") {
+        moveBy(1, 0);
+      }
+    };
+
+    window.addEventListener("keydown", listener);
+    return () => window.removeEventListener("keydown", listener);
+  }, [playerPosition, systems]);
 
   return (
     <div className="inline-grid p-2 bg-lime-500">
