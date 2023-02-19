67b335a1c856ce3900244e5fa03b5ec7894b8d10
ELENA_BURGER <elena96b@gmail.com>
2023-02-19 16:37:41 -0500

    2/3 through 5

--- packages/client/src/GameBoard.tsx
+++ packages/client/src/GameBoard.tsx
@@ -1,7 +1,6 @@
-import { useEffect } from "react";
 import { useComponentValue } from "@latticexyz/react";
-import { uuid } from "@latticexyz/utils";
 import { useMUD } from "./MUDContext";
+import { useMovement } from "./useMovement";
 
 export const GameBoard = () => {
   const rows = new Array(10).fill(0).map((_, i) => i);
@@ -14,51 +13,7 @@ export const GameBoard = () => {
   } = useMUD();
 
   const playerPosition = useComponentValue(Position, playerEntity);
-
-  const moveTo = async (x: number, y: number) => {
-    const positionId = uuid();
-    Position.addOverride(positionId, {
-      entity: playerEntity,
-      value: { x, y },
-    });
-
-    try {
-      const tx = await systems["system.Move"].executeTyped({ x, y });
-      await tx.wait();
-    } finally {
-      Position.removeOverride(positionId);
-    }
-  };
-
-  useEffect(() => {
-    const moveBy = async (deltaX: number, deltaY: number) => {
-      if (!playerPosition) {
-        console.warn(
-          "cannot moveBy without a player position, not yet spawned?"
-        );
-        return;
-      }
-      await moveTo(playerPosition.x + deltaX, playerPosition.y + deltaY);
-    };
-
-    const listener = (e: KeyboardEvent) => {
-      if (e.key === "ArrowUp") {
-        moveBy(0, -1);
-      }
-      if (e.key === "ArrowDown") {
-        moveBy(0, 1);
-      }
-      if (e.key === "ArrowLeft") {
-        moveBy(-1, 0);
-      }
-      if (e.key === "ArrowRight") {
-        moveBy(1, 0);
-      }
-    };
-
-    window.addEventListener("keydown", listener);
-    return () => window.removeEventListener("keydown", listener);
-  }, [playerPosition, systems]);
+  const { moveTo } = useMovement();
 
   return (
     <div className="inline-grid p-2 bg-lime-500">
@@ -66,17 +21,17 @@ export const GameBoard = () => {
         columns.map((x) => (
           <div
             key={`${x},${y}`}
-            className="w-8 h-8"
+            className="w-8 h-8 flex items-center justify-center cursor-pointer hover:ring"
             style={{
               gridColumn: x + 1,
               gridRow: y + 1,
             }}
-            onClick={async () => {
-              await moveTo(x,y)
+            onClick={(event) => {
+              event.preventDefault();
+              moveTo(x, y);
             }}
           >
-      {playerPosition?.x === x && playerPosition?.y === y ? <>🤠</> : null}
-
+            {playerPosition?.x === x && playerPosition?.y === y ? <>🤠</> : null}
           </div>
         ))
       )}
