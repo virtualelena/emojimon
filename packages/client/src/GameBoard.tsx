5605cef800edd805f34ff5008ec7399fa7970d46
ELENA_BURGER <elena96b@gmail.com>
2023-02-19 22:06:02 -0500

    mud city bitch

--- packages/client/src/GameBoard.tsx
+++ packages/client/src/GameBoard.tsx
@@ -5,9 +5,9 @@ import { useJoinGame } from "./useJoinGame";
 import { useMapConfig } from "./useMapConfig";
 
 export const GameBoard = () => {
-  const { width, height } = useMapConfig();
-  const rows = new Array(height).fill(0).map((_, i) => i);
-  const columns = new Array(width).fill(0).map((_, i) => i);
+  const mapConfig = useMapConfig();
+  const rows = new Array(mapConfig.height).fill(0).map((_, i) => i);
+  const columns = new Array(mapConfig.width).fill(0).map((_, i) => i);
 
   const {
     components: { Position },
@@ -21,26 +21,43 @@ export const GameBoard = () => {
   return (
     <div className="inline-grid p-2 bg-lime-500">
       {rows.map((y) =>
-        columns.map((x) => (
-          <div
-            key={`${x},${y}`}
-            className={`w-8 h-8 flex items-center justify-center ${
-              canJoinGame ? "cursor-pointer hover:ring" : ""
-            }`}
-            style={{
-              gridColumn: x + 1,
-              gridRow: y + 1,
-            }}
-            onClick={(event) => {
-              event.preventDefault();
-              if (canJoinGame) {
-                joinGame(x, y);
-              }
-            }}
-          >
-            {playerPosition?.x === x && playerPosition?.y === y ? <>🤠</> : null}
-          </div>
-        ))
+        columns.map((x) => {
+          const terrain = mapConfig.terrainValues.find(
+            (t: { x: number; y: number; }) => t.x === x && t.y === y
+          )?.type;
+
+          return (
+            <div
+              key={`${x},${y}`}
+              className={`w-8 h-8 flex items-center justify-center ${
+                canJoinGame ? "cursor-pointer hover:ring" : ""
+              }`}
+              style={{
+                gridColumn: x + 1,
+                gridRow: y + 1,
+              }}
+              onClick={(event) => {
+                event.preventDefault();
+                if (canJoinGame) {
+                  joinGame(x, y);
+                }
+              }}
+            >
+              <div className="flex flex-wrap gap-1 items-center justify-center relative">
+                {terrain ? (
+                  <div className="absolute inset-0 flex items-center justify-center text-3xl pointer-events-none">
+                    {terrain.emoji}
+                  </div>
+                ) : null}
+                <div className="relative">
+                  {playerPosition?.x === x && playerPosition?.y === y ? (
+                    <>🤠</>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          );
+        })
       )}
     </div>
   );
--- packages/client/src/useMapConfig.ts
+++ packages/client/src/useMapConfig.ts
@@ -1,5 +1,7 @@
 import { getComponentValue } from "@latticexyz/recs";
+import { ethers } from "ethers";
 import { useMUD } from "./MUDContext";
+import { terrainTypes, TerrainType } from "./terrainTypes";
 
 export const useMapConfig = () => {
   const {
@@ -12,6 +14,16 @@ export const useMapConfig = () => {
   if (mapConfig == null) {
     throw new Error("game config not set or not ready, only use this hook after loading state === LIVE");
   }
+
+  const { width, height, terrain } = mapConfig;
+  const terrainValues = Array.from(ethers.utils.toUtf8Bytes(terrain)).map((value, index) => ({
+    x: index % width,
+    y: Math.floor(index / width),
+    value,
+    type: value in TerrainType ? terrainTypes[value as TerrainType] : null,
+  }));
+
+  return { width, height, terrain, terrainValues };
 
   return mapConfig;
 };
